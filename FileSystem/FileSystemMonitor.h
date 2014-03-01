aff54d9cec348ce2a61762d13d6e2fd9f4f641c8
Kaf29-ASU <kaf29forgit@mail.ru>
2014-03-01 11:46:41 +0400

    added prototypes for functions in monitor

--- FileSystem/FileSystemMonitor.h
+++ FileSystem/FileSystemMonitor.h
@@ -4,5 +4,17 @@ class FileSystemMonitor
 {
 public:
 	FileSystem* filesystem;
+	void createNewFileSystem();
+	void showTableOfContents();
+	void showInformation();
+	void createNewFile();
+	void deleteFile();
+	void reNameFile();
+	void addInformationToFile();
+	void compressionOfFileSystem();
+	void changeFileType();
+	void writeVolumeLabelAndOwner();
+	void showTableInAlphavit();
+
 
 };
