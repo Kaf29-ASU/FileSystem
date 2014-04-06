b53f12a180980e74c0998ba30093e49d05798744
SloFy <hunter1623@mail.ru>
2014-04-07 01:06:15 +0400

    Потихонечку начал писать добавление в файл

--- FileSystem/addInformationToFile.cpp
+++ FileSystem/addInformationToFile.cpp
@@ -2,41 +2,46 @@
 
 int FileSystem::addInformationToFile()
 {//Обозначаем переменные
-	string fileName;
+	string FileName;
 	int InformationCount;//Кол-во добавляемой информации, 
 	int resultCode;
 	resultCode = 1;
-	int rez = 16; //Ограничение размера имени файла
-	int BlockNf, SegmNf,BlockNs,SegmNs; //Номер блока , номер сегмента
+	int lenCritical =16;
+	int CntCritical = 2 ^ 9;
+	int BlockNf, SegmNf, BlockNs, SegmNs; //Номер блока , номер сегмента
 	FileDescriptor AddFile;
 	FileDescriptor NextFile;
 	int freeSpace; //кол-во свободных блоков перед следующим файлом
 
 	//Собствено функция
 	cout << "Введите имя файла" << endl;
-	cin >> fileName;
+	cin >> FileName;
+	if (FileName.length > lenCritical)//Проверка ввода имени
+		return(1);
+
 	cout << "Введите кол-во добавляемой информации" << endl;
 	cin >> InformationCount;
-
-	if (InformationCount > rez)//Проверка ввода
+	if (InformationCount > CntCritical)//Проверка ввода кол-ва информации 
 		return(1);
-	AddFile = getRecord(fileName);
+
+	AddFile = getRecord(FileName);
 	if ((AddFile.fileType) == "")
 		return(1);
-	NextFile = getRecord(fileName);//Тут нужно бы захуярить еще поиск следющего файла, так-то
-
-
-
-
-
-
+	NextFile = getRecord(FileName);//Тут нужно бы захуярить еще поиск следющего файла, так-то
 	
 
-		return(resultCode);
 
-{
-	int resultCode=0;
 
+
+
+
+	//AddFile.blockCount += InformationCount; //Увеличим кол-во инф. в файле
 	return(resultCode);
-
 }
+
+//{
+//	int resultCode=0;
+//
+//	return(resultCode);
+//
+//}
