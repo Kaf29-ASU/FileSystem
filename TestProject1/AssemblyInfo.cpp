c448ab0c96ebae152af6f8f88febab13cd4a0b44
Kaf29-ASU <kaf29forgit@mail.ru>
2014-04-15 10:51:43 +0400

    первая пара....

---
+++ TestProject1/AssemblyInfo.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+
+//
+// Общие сведения об этой сборке предоставляются следующим набором
+// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+// общие сведения об этой сборке.
+//
+[assembly:AssemblyTitleAttribute("TestProject1")];
+[assembly:AssemblyDescriptionAttribute("")];
+[assembly:AssemblyConfigurationAttribute("")];
+[assembly:AssemblyCompanyAttribute("")];
+[assembly:AssemblyProductAttribute("TestProject1")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c)  2014")];
+[assembly:AssemblyTrademarkAttribute("")];
+[assembly:AssemblyCultureAttribute("")];
+
+//
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию,
+// используя "*", как показано ниже:
+
+[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:ComVisible(false)];
+
