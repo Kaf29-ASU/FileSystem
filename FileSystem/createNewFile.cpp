8bf946cd0a03fd1dba2096caa52c97f5a8f7082a
Kaf29-ASU <kaf29forgit@mail.ru>
2014-04-17 16:59:02 +0400

    написал проверку входных для содания файла

--- FileSystem/createNewFile.cpp
+++ FileSystem/createNewFile.cpp
@@ -6,19 +6,42 @@ int FileSystem::createNewFile()
 	string fileName;
 	cout<<"Введите имя файла не превышающее 48 символа"<<endl;
 	cin>>fileName;
+		if(fileName.length()>48)
+		{
+			resultCode=2222;
+			return(resultCode);
+		};
+	
+	if(getRecordNumber(fileName)!=0)
+		{
+			resultCode=222;
+			return(resultCode);
+		};
 	string fileType;
 	cout<<"Введите тип файла не превышающий 32 сиволов"<<endl;
 	cin>>fileType;
+		if(fileType.length()>16)
+	{
+		resultCode=2222;
+		return(resultCode);
+	};
 	int fileSize;
 	cout<<"Введите размер файла в байтах"<<endl;
 	cin>>fileSize;
 	string fileSizeString=toString(fileSize, 16);
+	cin>>fileType;
+		if(fileSizeString.length()>16)
+	{
+		resultCode=2222;
+		return(resultCode);
+	};
 	char creationDate[16];
 	time_t seconds = time(NULL);
 	tm* timeinfo = localtime(&seconds);
 	char* format = "%B %d, %Y";
 	strftime(creationDate, 16, format, timeinfo);
 	cout<<endl<<"Дата создания файла: "<<creationDate<<endl;
+	
 	FileDescriptor fileDescriptor;
 	fileDescriptor.fileName=fileName;
 	fileDescriptor.fileType=fileType;
