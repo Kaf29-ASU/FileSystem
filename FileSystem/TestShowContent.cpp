bef3ff9919df63ccf23662f44bf51cce6d7f7119
Kaf29-ASU <kaf29forgit@mail.ru>
2014-04-22 11:48:13 +0400

    тест на вывод оглавления

--- FileSystem/TestShowContent.cpp
+++ FileSystem/TestShowContent.cpp
@@ -9,13 +9,13 @@
 
 
 
-/*class TestFileSystemMonitor : public ::testing::Test {
+class TestFileSystemMonitor : public ::testing::Test {
 public:
 	void SetUp()	// инициализация тестируемого класса
 	{
 		f1=new FileSystemMonitor;
-		f1->fileSystem.createFile("testMonitor");
-		f1->fileSystem.openFile("testMonitor");
+		//f1->fileSystem.createFile("");
+		f1->fileSystem.openFile("testContent");
 		
 	}
 	FileSystemMonitor *f1;
@@ -23,18 +23,21 @@ public:
 	
 };
 
-TEST_F(TestFileSystemMonitor, correctWork)
+TEST_F(TestFileSystemMonitor, showContent)
 {
+	setlocale(LC_ALL, "Russian"); 
 	stringstream oss;
+	oss.clear();
 	std::cout.rdbuf(oss.rdbuf());
 	//std::cout<<"test";
 	//return;
 	//std::stringstream s;
 	 //#define cout s;
-	f1->deleteFile();
+	f1->fileSystem.showTableOfContent();
 	string ss;
 	ss = oss.str();
-	string s1="";
+	string s1="testtest.ttt\n";
+	s1=s1+ "1000000\n" +"Апрель 18, 20140\n\n"+"ttttt.ttt\n"+"100000000\n"+"Апрель 18, 20140\n\n";
+	
 	EXPECT_EQ(ss,s1);
 }
-*/
