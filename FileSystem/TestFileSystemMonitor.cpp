5f3093672fb2a65e6dbf7d7b921ea0aabb20410f
Kaf29-ASU <kaf29forgit@mail.ru>
2014-04-22 10:39:32 +0400

    УРААА тест сработал!!!

--- FileSystem/TestFileSystemMonitor.cpp
+++ FileSystem/TestFileSystemMonitor.cpp
@@ -3,6 +3,9 @@
 //#include <gtest\gtest.h>
 #include <gtest\gtest.h>
 #include <iomanip>
+#include <sstream>
+#include <iostream>
+
 
 
 
@@ -13,7 +16,24 @@ public:
 		f1=new FileSystemMonitor;
 		f1->fileSystem.createFile("testMonitor");
 		f1->fileSystem.openFile("testMonitor");
+		
 	}
 	FileSystemMonitor *f1;
+	
+	
 };
 
+TEST_F(TestFileSystemMonitor, correctWork)
+{
+	stringstream oss;
+	std::cout.rdbuf(oss.rdbuf());
+	//std::cout<<"test";
+	//return;
+	//std::stringstream s;
+	 //#define cout s;
+	f1->deleteFile();
+	string ss;
+	ss = oss.str();
+	string s1="";
+	EXPECT_EQ(ss,s1);
+}
